686212c0f32e5f3a381c937d7108e1c4d473ba45
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 14:27:41 +0100

    Update streamlit_app.py
    added fruity vice functionality

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import requests
 
 st.title('My Parents\' New Healthy Dinner')
 st.header('Breakfast Menu')
@@ -16,3 +17,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit')
 fruits_selected = st.multiselect("Pick some fruits:", list(my_fruit_list.index))
 fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
+
+st.header('Fruity Vice Fruity Advice!')
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/{fruits_selected}")
+st.text(fruityvice_response.json())
