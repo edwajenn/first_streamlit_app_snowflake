85e70baff089d6972d5099e08686d387cc7cc239
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 10:08:53 +0100

    Update streamlit_app.py
    fixed some grammar

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents\' New Healthy Dinner')
 streamlit.header('Breakfast Menu')
 streamlit.text('Porridge with Pink Lady Apple')
 streamlit.text('Beans on Toast with Extra Anchovy')
