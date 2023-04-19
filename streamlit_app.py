b811176579904f6e6916a23cba5daecdc368b3aa
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 10:27:56 +0100

    Update streamlit_app.py
    added fruit index
    added multi-select option

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,17 @@
-import streamlit
+import streamlit as st
 import pandas as pd
-streamlit.title('My Parents\' New Healthy Dinner')
-streamlit.header('Breakfast Menu')
-streamlit.text('🥣Porridge with Pink Lady Apple')
-streamlit.text('🍞Beans on Toast with Extra Anchovy')
-streamlit.text('🐔Soft-Boiled Ostrich Egg')
-streamlit.text("""🍌Banana with Extra Sausages""")
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+st.title('My Parents\' New Healthy Dinner')
+st.header('Breakfast Menu')
+st.text('🥣Porridge with Pink Lady Apple')
+st.text('🍞Beans on Toast with Extra Anchovy')
+st.text('🐔Soft-Boiled Ostrich Egg')
+st.text("""🍌Banana with Extra Sausages""")
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-streamlit.dataframe(my_fruit_list)
+my_fruit_list = my_fruit_list.set_index('Fruit')
+
+# inserting multi-select option
+st.multiselect("Pick some fruits:", list(my_fruit_list.index))
+st.dataframe(my_fruit_list)
