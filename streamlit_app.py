31ec26ab536fd1d652e37a14314319d52531cec9
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 16:34:32 +0100

    Update streamlit_app.py
    corrected syntax

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,4 +37,4 @@ st.dataframe(my_data_rows)
 add_my_fruit = st.text_input('What fruit would you like to add?')
 st.write('Thank you for adding', add_my_fruit)
 
-my_cur.execute("Insert into fruit_load_list values ('from streamlit')"
+my_cur.execute("Insert into fruit_load_list values ('from streamlit')")
