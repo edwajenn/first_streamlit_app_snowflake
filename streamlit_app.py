ed014b360496ce294bb714d08d9e94b14438e9cf
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 16:43:04 +0100

    Update streamlit_app.py
    tarting it up a bit with a try/else

--- streamlit_app.py
+++ streamlit_app.py
@@ -21,14 +21,21 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header('Fruity Vice Fruity Advice!')
-fruit_choice = st.text_input('What fruit would you like information about?', 'Kiwi')
-st.write('The user entered', fruit_choice)
+try:
+  fruit_choice = st.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    st.error("Please choose a pre-existing fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    st.dataframe(fruityvice_normalized)
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-st.dataframe(fruityvice_normalized)
-
+    except URLError as e:
+      st.error()
+    
+    
 st.stop()
+
 # Start of Snowflake
 my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
 my_cur = my_cnx.cursor()
