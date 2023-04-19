760c1db6009855f9300c796e45dad5537042995a
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 14:32:59 +0100

    Update streamlit_app.py
    removed attempt at f-string -- too rusty
    added dataframe

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,5 +19,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 st.dataframe(fruits_to_show)
 
 st.header('Fruity Vice Fruity Advice!')
-fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruits_selected}")
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 st.text(fruityvice_response.json())
+
+fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+st.dataframe(fruityvice_normalized)
