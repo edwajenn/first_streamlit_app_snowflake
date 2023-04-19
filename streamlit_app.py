29649b6a99e7d7dd9a9c6b399accd8c3d656fffb
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 10:08:07 +0100

    Update streamlit_app.py
    added breakfast menu
    added breakfast items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Dinner')
+streamlit.header('Breakfast Menu')
+streamlit.text('Porridge with Pink Lady Apple')
+streamlit.text('Beans on Toast with Extra Anchovy')
+streamlit.text('Soft-Boiled Ostrich Egg')
