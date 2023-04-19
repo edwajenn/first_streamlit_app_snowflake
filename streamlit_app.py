3c1d69dcb15712cc7a5e3106d973f2282a063719
edwajenn <118005714+edwajenn@users.noreply.github.com>
2023-04-19 10:13:30 +0100

    Update streamlit_app.py
    added smoothie option if they don't like anchovies

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@ import streamlit
 
 streamlit.title('My Parents\' New Healthy Dinner')
 streamlit.header('Breakfast Menu')
-streamlit.text('🥣 Porridge with Pink Lady Apple')
+streamlit.text('🥣Porridge with Pink Lady Apple')
 streamlit.text('🍞Beans on Toast with Extra Anchovy')
 streamlit.text('🐔Soft-Boiled Ostrich Egg')
 streamlit.text("""🍌Banana with Extra Sausages""")
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
